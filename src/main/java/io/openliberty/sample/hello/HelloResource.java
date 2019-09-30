a3d295b0872de7d33cdaaacfd284cd7d8ab040d0
Sebastian Daschner <git@sebastian-daschner.de>
2019-09-30 10:13:35 +0300

    updated

--- src/main/java/io/openliberty/sample/hello/HelloResource.java
+++ src/main/java/io/openliberty/sample/hello/HelloResource.java
@@ -2,6 +2,7 @@ package io.openliberty.sample.hello;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
 
 @Path("hello")
 public class HelloResource {
@@ -11,5 +12,10 @@ public class HelloResource {
         return "Hello!";
     }
 
+    @GET
+    @Path("fail")
+    public String fail() {
+        throw new WebApplicationException(500);
+    }
 
 }
